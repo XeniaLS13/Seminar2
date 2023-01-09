de96428824c2392a05ae378029ed911e535fde44
XeniaLS13 <kseniavagabova@mail.ru>
2023-01-09 03:40:01 +0300

    В ходе семинара созданы варианты решения для задачи по написанию программы

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿//Написать программу, которая на вход получает массив, 
+//а на выходе собирает новый массив только из чисел, кратных 5.
+int[] arr = {2, 5, 6, 10, 32, 30};
+
+int count = 0;
+int index = 0;
+
+while (index < arr.Length)
+{
+    if (arr[index] % 5 == 0)
+    {
+        count++;
+    }
+    index++;
+}
+int[] arrNew = new int[count];
+index = 0;
+int IndexNew = 0;
+
+while (index < arr.Length)
+{
+    if (arr[index] % 5 == 0)
+
+    {
+        arrNew[IndexNew] = arr[index];
+        IndexNew++;
+    }
+    index++;
+}
+Console.WriteLine(arrNew);
+index = 0;
+while (index < arrNew.Length)
+{
+    System.Console.WriteLine(arrNew[index]);
+    index++;
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿// //Написать программу, которая на вход получает массив, 
+//а на выходе собирает новый массив только из чисел, кратных 5.
+int[] CreateCollection(int size)
+{
+    int[] collection = new int[size];
+    int index = 0;
+    while (index < size)
+    {
+        collection[index] = new Random().Next(20);
+        index = index + 1;
+    }
+    return collection;
+}
+int SolutionExercise(int[] array)
+{
+    int currentPosition = 0;
+    int sum = 0;
+    int Length = array.Length;
+
+    while (currentPosition < Length)
+    {
+        int currentElement = array[currentPosition];
+        if (currentElement % 5 = 0)
+        {
+            sum = sum + currentElement;
+        }
+        currentPosition = currentPosition + 1;
+    }
+    return sum;
+}
+
+int[] array = CreateCollection(size: 10);
+int sum = SolutionExercise(array);
+Console.WriteLine(sum);
